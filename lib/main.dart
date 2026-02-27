94ef4b7a4c2d6946491ffe515f6cb4fb43b1ff8d
Hrushikesh <hrushikeshrv@gmail.com>
2026-02-27 11:35:29 -0800

    Run on iOS device

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    name: "Money Warden",
+    name: "MoneyWarden",
     options:DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MoneyWarden());
